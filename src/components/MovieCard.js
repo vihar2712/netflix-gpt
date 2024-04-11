10b2f0f741108c0f63ae2f6af2675e421bf6e5d5
vihar2712 <viharshah222@gmail.com>
2024-04-11 16:43:48 +0530

    changed movie card dimensions for mobile

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,7 +5,7 @@ const MovieCard = ({ imageId, movieId }) => {
   if (!imageId) return;
   return (
     <Link to={"/movies/" + movieId}>
-      <div className="w-12 h-24 sm:w-36 sm:h-48 md:w-42 md:h-56 lg:w-48 lg:h-64 xl:w-52 xl:h-80 2xl:w-64 2xl:h-96 hover:scale-105 hover:cursor-pointer">
+      <div className="w-24 h-36 sm:w-36 sm:h-48 md:w-42 md:h-56 lg:w-48 lg:h-64 xl:w-52 xl:h-80 2xl:w-64 2xl:h-96 hover:scale-105 hover:cursor-pointer">
         {/* {showName && (
         <h1 className="text-md">
           {title}
